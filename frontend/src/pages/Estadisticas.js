8da730f580b5c1ee2ef80d241c214c329b99e770
Roberto Arechavala <robertoarechavalapuertas@gmail.com>
2025-11-19 13:48:01 -0300

    Rutas y páginas iniciales creadas del frontend

---
+++ frontend/src/pages/Estadisticas.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+
+export default function Equipos() {
+  return <h1>Página de Estadisticas</h1>;
+}
---
+++ frontend/src/router/AppRouter.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import Equipos from "../pages/Equipos";
+import Jugadores from "../pages/Jugadores";
+import Partidos from "../pages/Partidos";
+import Estadisticas from "../pages/Estadisticas";
+
+export default function AppRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/equipos" element={<Equipos />} />
+        <Route path="/jugadores" element={<Jugadores />} />
+        <Route path="/partidos" element={<Partidos />} />
+        <Route path="/estadisticas" element={<Estadisticas />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
